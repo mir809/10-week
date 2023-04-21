0e76ee00ab84371e080cf03d6620655a771db470
mir809 <humir809@naver.com>
2023-04-21 21:41:10 +0900

    바닐라 JS  #2.13~#3.8 과제 (04/21)

--- challenge/src/index.js
+++ challenge/src/index.js
@@ -1,30 +1,17 @@
-const h2 = document.querySelector("h2");
-const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+const background = document.querySelector("body");
 
-const superEventHandler = {
-  handleEnter: function () {
-    h2.innerText = "The mouse is here!";
-    h2.style.color = colors[0];
-  },
-  handleLeave: function () {
-    h2.innerText = "The mouse is gone!";
-    h2.style.color = colors[1];
-  },
-  handleResize: function () {
-    h2.innerText = "You just resized!";
-    h2.style.color = colors[2];
-  },
-  handleSelect: function () {
-    h2.innerText = "You're selecting me!";
-    h2.style.color = colors[3];
-  },
-  handleContext: function () {
-    h2.innerHTML = "That was a right click!";
-    h2.style.color = colors[4];
-  },
-};
+function resize() {
+  const width = window.innerWidth;
 
-h2.addEventListener("mouseenter", superEventHandler.handleEnter);
-h2.addEventListener("mouseleave", superEventHandler.handleLeave);
-window.addEventListener("resize", superEventHandler.handleResize);
-window.addEventListener("contextmenu", superEventHandler.handleContext);
+  if (width <= 600) {
+    background.style.backgroundColor = "skyblue";
+  } else if (width > 600 && width <= 900) {
+    background.style.backgroundColor = "violet";
+  } else {
+    background.style.backgroundColor = "orange";
+  }
+}
+
+resize();
+
+window.addEventListener("resize", resize);
