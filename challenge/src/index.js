b38dc4d6205eadf951b2b90ef722494691b20f4a
mir809 <humir809@naver.com>
2023-04-25 10:42:28 +0900

    바닐라 JS #4.0~#4.7 과제 모범답안

--- challenge/src/index.js
+++ challenge/src/index.js
@@ -1,34 +1,22 @@
-const btn = document.querySelector("button");
-const generate = document.querySelector(".generate");
-const guess = document.querySelector(".guess");
+const guessForm = document.getElementById("js-guess");
+const result = document.getElementById("js-result");
+const maxNumber = document.getElementById("maxNumber");
 
-const you = document.querySelector(".select-number");
-const machine = document.querySelector(".random-number");
-const victory = document.querySelector(".victory");
-
-const hidden = document.querySelector(".hidden");
-
-hidden.style.display = "none";
-
-function result(event) {
-  event.preventDefault();
-  hidden.style.display = "contents";
-
-  const genNum = parseInt(generate.value);
-  const guessNum = parseInt(guess.value);
-
-  const random = parseInt(Math.ceil(Math.random() * genNum));
-
-  you.innerText = guessNum;
-  machine.innerText = random;
-
-  if (guessNum === random) {
-    victory.innerText = "You win!";
-    victory.style.fontWeight = "600";
-  } else {
-    victory.innerText = "You lost!";
-    victory.style.fontWeight = "100";
+function handleGuessSubmit(e) {
+  e.preventDefault();
+  const guessInput = guessForm.querySelector("input");
+  if (guessInput.value === "" && maxNumber === "") {
+    return;
   }
+  const max = maxNumber.value;
+  const random = Math.ceil(Math.random() * max);
+  const userGuess = parseInt(guessInput.value, 10);
+  const resultSpan = result.querySelector("span");
+  resultSpan.innerHTML = `
+  You chose: ${userGuess},
+  the machine chose: ${random}.<br />
+  <strong>${userGuess === random ? "You won!" : "You lost!"}</strong>
+  `;
 }
 
-btn.addEventListener("click", result);
+guessForm.addEventListener("submit", handleGuessSubmit);
