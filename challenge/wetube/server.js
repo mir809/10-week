5d50623d408d4e1c7fd34e36fb9a3bfad6be442d
mir809 <humir809@naver.com>
2023-05-03 15:47:02 +0900

    유튜브 #3.5 to #3.10 과제 (05.03)

--- challenge/wetube/server.js
+++ challenge/wetube/server.js
@@ -2,9 +2,41 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("<h1>Home</h1>"));
-app.get("/about", (req, res) => res.send("<h1>About</h1>"));
-app.get("/contact", (req, res) => res.send("<h1>Contact</h1>"));
-app.get("/login", (req, res) => res.send("<h1>Login</h1>"));
+let now = new Date();
+let year = now.getFullYear();
+let month = now.getMonth();
+let day = now.getDate();
 
-app.listen(() => `Listening!✅`);
+const url = (req, res, next) => {
+  console.log(`Path : ${req.url}`);
+  next();
+};
+
+const time = (req, res, next) => {
+  console.log(`Time : ${year}년 ${month}월 ${day}일`);
+  next();
+};
+
+const security = (req, res, next) => {
+  if (req.protocol === "https") {
+    console.log(`Now Protocol : ${req.protocol} => secure`);
+  } else {
+    console.log(`Now Protocol : ${req.protocol} => insecure`);
+  }
+  next();
+};
+
+const protector = (req, res, next) => {
+  if (req.url === "/protected") {
+    return res.redirect("/");
+  }
+  next();
+};
+
+app.use(url, time, security, protector);
+
+app.get("/", (req, res) => res.send("<h1>Home</h1>"));
+app.get("/protected", (req, res) => res.send("<h1>Protected</h1>"));
+
+// Codesandbox gives us a PORT :)
+app.listen(process.env.PORT, () => `Listening!✅`);
