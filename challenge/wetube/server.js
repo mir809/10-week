bc7ff481bc1f7b404ac15b00d7a76bc701c1b11e
mir809 <humir809@naver.com>
2023-05-03 14:43:12 +0900

    유튜브 #3.0 ~ #3.4과제 모범답안

--- challenge/wetube/server.js
+++ challenge/wetube/server.js
@@ -1,27 +1,10 @@
 import express from "express";
 
-const PORT = 4000;
-
 const app = express();
 
-const home = (req, res) => {
-  return res.send("<h1>Home</h1>");
-};
-const about = (req, res) => {
-  return res.send("<h1>About</h1>");
-};
-const contact = (req, res) => {
-  return res.send("<h1>Contact</h1>");
-};
-const login = (req, res) => {
-  return res.send("<h1>Login</h1>");
-};
+app.get("/", (req, res) => res.send("<h1>Home</h1>"));
+app.get("/about", (req, res) => res.send("<h1>About</h1>"));
+app.get("/contact", (req, res) => res.send("<h1>Contact</h1>"));
+app.get("/login", (req, res) => res.send("<h1>Login</h1>"));
 
-app.get("/", home);
-app.get("/about", about);
-app.get("/contact", contact);
-app.get("/login", login);
-
-const listen = () => console.log("서버 연결중");
-
-app.listen(PORT, listen);
+app.listen(() => `Listening!✅`);
