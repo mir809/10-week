e37532a636888c08dc63bd553472bdd2ad39a504
mir809 <humir809@naver.com>
2023-05-12 07:05:59 +0900

    유튜브 #6.0 ~ #6.6 과제 모범답안
    https://codesandbox.io/s/a09solution-lwmj2

--- challenge/wetube/src/movieController.js
+++ challenge/wetube/src/movieController.js
@@ -7,26 +7,25 @@ export const movieDetail = (req, res) => {
   const {
     params: { id }
   } = req;
-
   const movie = getMovieById(id);
   if (!movie) {
     res.render("404", { pageTitle: "Movie not found" });
   }
-  console.log(movie.genres);
-  return res.render("detail", { movie });
+  return res.render("detail", { movie, pageTitle: movie.title });
 };
 
-export const getAdd = (req, res) => {
-  return res.render("add", { pageTitle: "Add Movie" });
-};
-
-export const postAdd = (req, res) => {
-  const { title, synopsis, genres } = req.body;
-  const newVideo = {
-    title,
-    synopsis,
-    genres: genres.split(",")
-  };
-  addMovie(newVideo);
-  return res.redirect("/");
+/*
+Write the controller or controllers you need to render the form
+and to handle the submission
+*/
+export const add = (req, res) => {
+  if (req.method === "GET") {
+    return res.render("add", { pageTitle: "Add Movie" });
+  } else if (req.method === "POST") {
+    const {
+      body: { title, genres, synopsis }
+    } = req;
+    addMovie({ title, synopsis, genres: genres.split(",") });
+    return res.redirect("/");
+  }
 };
