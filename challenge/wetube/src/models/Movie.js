d7533008a43bcaf4b49c00aeb2fde3ae46c3e069
mir809 <humir809@naver.com>
2023-05-15 10:16:20 +0900

    유튜브 #6.11 ~ #6.28 과제 (05.15)
    https://codesandbox.io/s/a11blueprint-forked-8getuy?file=/src/movieController.js

--- challenge/wetube/src/models/Movie.js
+++ challenge/wetube/src/models/Movie.js
@@ -1,6 +1,15 @@
 import mongoose from "mongoose";
 
-// Create a Movie Model here.
+/*
+PLEASE ADD YOUR USERNAME IN THIS LINE.
+ALL THE MODELS YOU WILL CREATE WILL HAVE YOUR USERNAME APPENDED TO THEM
+SO YOU CAN SEARCH / ADD / EDIT / DELETE YOUR DOCUMENTS ONLY.
+PLEASE FOLLOW THIS STEP
+WE NEED TO SHARE THE SAME DB SO NICO CAN CHECK OUT EVERYBODYS PROJECT.
+🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
+*/
+const YOUR_USERNAME = "user";
+
 const MovieSchema = mongoose.Schema({
   title: {
     type: String,
@@ -18,14 +27,30 @@ const MovieSchema = mongoose.Schema({
     type: Number,
     required: true
   },
-  genres: [
-    {
-      type: String,
-      required: true
-    }
-  ]
+  genres: [String]
 });
 
-const Movie = mongoose.model("Movie", MovieSchema);
+if (YOUR_USERNAME === null || typeof YOUR_USERNAME !== "string") {
+  /*
+  PLEASE ADD YOUR USERNAME ON THE LINE 10
+  THIS LINE WILL REMIND YOU IF YOU HAVEN'T ADDED IT
+  PLEASE DONT REMOVE THIS LINE
+  */
+  throw Error(
+    "❌  Please add your username in the line 10 of models/Movie.js  ❌"
+  );
+}
 
-export default Movie;
+if (YOUR_USERNAME.includes("@")) {
+  throw Error("❌  Please remove the @ from your username  ❌");
+}
+
+MovieSchema.static("formatGenres", function (genres) {
+  return genres
+    .split(",")
+    .map((word) => (word.startsWith(` `) ? word : ` ${word}`));
+});
+
+const model = mongoose.model(`Movie_${YOUR_USERNAME}`, MovieSchema);
+
+export default model;
