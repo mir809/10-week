77df9328fbe8a53e462a4aec9d15dffb3d36fd1d
mir809 <humir809@naver.com>
2023-05-23 20:43:25 +0900

    유튜브  #8.6 ~ #8.9 과제 (05.23)
    https://codesandbox.io/s/a15blueprint-forked-u94c58?file=/src/index.js

--- challenge/wetube/src/index.js
+++ challenge/wetube/src/index.js
@@ -1,25 +1,72 @@
-import "./db";
-import "./models/User";
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
-import session from "express-session";
-import userRouter from "./userRouter";
-import { localsMiddleware } from "./middlewares";
+import multer from "multer";
+import fs from "fs";
 
 const app = express();
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true
-  })
-);
-app.use(localsMiddleware);
-app.use("/", userRouter);
+
+app.use("/uploads", express.static("uploads"));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1000000 // 파일 용량 제한: 1MB (단위: byte)
+  }
+});
+
+const home = (req, res) => {
+  fs.readdir(`uploads`, (err, files) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).end();
+    }
+    return res.render("home", { pageTitle: "TXT2HTML!", files });
+  });
+};
+
+const read = (req, res) => {
+  const { file } = req;
+  const path = file.path;
+
+  fs.readFile(path, "utf8", (err, data) => {
+    if (err) {
+      const errorMessage = "Failed Convert";
+      return res.render("read", { pageTitle: "READ TEXT FILE", errorMessage });
+    } else {
+      return res.render("read", { pageTitle: "READ TEXT FILE", data });
+    }
+  });
+};
+
+const getRead = (req, res) => {
+  const { id } = req.params;
+  const path = `uploads/` + id;
+
+  fs.readFile(path, "utf8", (err, data) => {
+    if (err) {
+      const errorMessage = "Failed Convert";
+      return res.render("read", { pageTitle: "READ TEXT FILE", errorMessage });
+    } else {
+      return res.render("read", { pageTitle: "READ TEXT FILE", data });
+    }
+  });
+};
+app.get("/", home);
+app.post("/read", upload.single("text"), read);
+app.get("/read/:id", getRead);
 
 // Codesanbox does not need PORT :)
-app.listen(() => console.log(`✅  Server Ready!`));
+app.listen();
