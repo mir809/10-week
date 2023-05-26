7e03861c5bd0dd4d202e17dc33939439d9bbe339
mir809 <humir809@naver.com>
2023-05-26 11:12:27 +0900

    유튜브#11.0 ~ #11.3 과 (05.26)
    https://codesandbox.io/s/a17blueprint-forked-xurmmm?file=/src/index.js

--- challenge/wetube/src/index.js
+++ challenge/wetube/src/index.js
@@ -1,57 +1,59 @@
-import express from "express";
-import path from "path";
-import bodyParser from "body-parser";
-import multer from "multer";
-import fs from "fs";
+const video = document.querySelector("video");
 
-const app = express();
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
-app.use(bodyParser.urlencoded({ extended: true }));
+const playBtn = document.getElementById("playBtn");
+const muteBtn = document.getElementById("muteBtn");
+const volumeRange = document.getElementById("volume");
 
-// Middlewares
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname + "-" + Date.now());
+let volumeValue = 0.5; // 볼륨 초기값
+video.volume = volumeValue; // 영상의 실제 소리
+
+// 영상 재생/일시정지
+const clickPlayBtn = () => {
+  video.paused ? video.play() : video.pause();
+  playBtn.innerText = video.paused ? "Play" : "Pause";
+}; //버튼 클릭시
+
+// 음량 - 음소거 버튼, 볼륨 막대
+const clickMuteBtn = () => {
+  if (video.muted) {
+    video.muted = false;
+    video.volume = volumeValue;
+  } else {
+    video.muted = true;
   }
-});
-
-const uploadFiles = multer({ storage });
-
-// Controllers
-const getText = (req, res) => {
-  fs.readdir("uploads", (err, data) => {
-    if (err) {
-      console.log(err);
-      throw err;
-    }
-    return res.render("home", { fileNameList: data });
-  });
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+  // 음소거 해제시 볼륨막대 위치가 음소거 하기 전으로 돌아감
 };
 
-const postText = (req, res) => {
-  const { filename } = req.file;
-  return res.redirect(`/read/${filename}`);
+const volumeInput = (event) => {
+  const {
+    target: { value } // 볼륨 막대를 조절한 값
+  } = event;
+  if (Number(value) === 0) {
+    muteBtn.innerText = "Unmute";
+    video.muted = true;
+  } else {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  video.volume = value; // 영상의 실제 소리
 };
 
-const showText = (req, res) => {
-  const { id } = req.params;
-  const path = "uploads/" + id;
-  fs.readFile(path, "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-      throw err;
-    }
-    return res.send(data);
-  });
+const volumeChange = (event) => {
+  const {
+    target: { value } // 볼륨 막대를 조절한 값
+  } = event;
+  if (Number(value) !== 0) {
+    volumeValue = value;
+  } // 볼륨조절 막대가 마지막에 멈춰있던 값을 기억함 (0일때 제외)
 };
 
-//Routers
-app.route("/").get(getText).post(uploadFiles.single("text"), postText);
-app.get("/read/:id", showText);
+playBtn.addEventListener("click", clickPlayBtn);
+video.addEventListener("click", clickPlayBtn);
+//동영상 일시정지, 시작
 
-// Codesanbox does not need PORT :)
-app.listen(() => console.log(`Listening!`));
+muteBtn.addEventListener("click", clickMuteBtn);
+volumeRange.addEventListener("input", volumeInput);
+volumeRange.addEventListener("change", volumeChange);
+//음소거&음소거 해제 , 볼륨조절
