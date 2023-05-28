1c179e78e5a4b7d0782b246cf84aab0e6808e38a
mir809 <humir809@naver.com>
2023-05-29 07:55:01 +0900

    유튜브#11.0 ~ #11.3 과제 모범답안
    https://codesandbox.io/s/a17solution-sbqmn

--- challenge/wetube/src/index.js
+++ challenge/wetube/src/index.js
@@ -1,59 +1,50 @@
 const video = document.querySelector("video");
+const videoController = document.getElementById("videoController");
+const psBtn = videoController.querySelector("#playPauseBtn");
+const volumeBtn = videoController.querySelector("#volume");
+const volumeRange = videoController.querySelector("#volumeRange");
 
-const playBtn = document.getElementById("playBtn");
-const muteBtn = document.getElementById("muteBtn");
-const volumeRange = document.getElementById("volume");
+let volumeValue = 0.5;
+video.volume = volumeValue;
 
-let volumeValue = 0.5; // 볼륨 초기값
-video.volume = volumeValue; // 영상의 실제 소리
+const handlePlayAndStop = () => {
+  if (video.paused) {
+    video.play();
+    psBtn.className = "fas fa-pause";
+  } else {
+    video.pause();
+    psBtn.className = "fas fa-play";
+  }
+};
 
-// 영상 재생/일시정지
-const clickPlayBtn = () => {
-  video.paused ? video.play() : video.pause();
-  playBtn.innerText = video.paused ? "Play" : "Pause";
-}; //버튼 클릭시
-
-// 음량 - 음소거 버튼, 볼륨 막대
-const clickMuteBtn = () => {
+const handleSound = () => {
   if (video.muted) {
     video.muted = false;
-    video.volume = volumeValue;
+    volumeRange.value = volumeValue;
+    volumeBtn.className = "fas fa-volume-up";
   } else {
     video.muted = true;
+    volumeRange.value = 0;
+    volumeBtn.className = "fas fa-volume-mute";
   }
-  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : volumeValue;
-  // 음소거 해제시 볼륨막대 위치가 음소거 하기 전으로 돌아감
 };
 
-const volumeInput = (event) => {
+const handleVolume = (event) => {
   const {
-    target: { value } // 볼륨 막대를 조절한 값
+    target: { value }
   } = event;
-  if (Number(value) === 0) {
-    muteBtn.innerText = "Unmute";
-    video.muted = true;
-  } else {
+  if (video.muted) {
     video.muted = false;
-    muteBtn.innerText = "Mute";
+    volumeBtn.className = "fas fa-volume-mute";
   }
-  video.volume = value; // 영상의 실제 소리
+  if (value === "0") {
+    volumeBtn.className = "fas fa-volume-off";
+  } else {
+    volumeBtn.className = "fas fa-volume-up";
+  }
+  video.volume = volumeValue = value;
 };
 
-const volumeChange = (event) => {
-  const {
-    target: { value } // 볼륨 막대를 조절한 값
-  } = event;
-  if (Number(value) !== 0) {
-    volumeValue = value;
-  } // 볼륨조절 막대가 마지막에 멈춰있던 값을 기억함 (0일때 제외)
-};
-
-playBtn.addEventListener("click", clickPlayBtn);
-video.addEventListener("click", clickPlayBtn);
-//동영상 일시정지, 시작
-
-muteBtn.addEventListener("click", clickMuteBtn);
-volumeRange.addEventListener("input", volumeInput);
-volumeRange.addEventListener("change", volumeChange);
-//음소거&음소거 해제 , 볼륨조절
+psBtn.addEventListener("click", handlePlayAndStop);
+volumeBtn.addEventListener("click", handleSound);
+volumeRange.addEventListener("input", handleVolume);
